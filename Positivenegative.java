525c9c7b329c56a9921a4d0126728f5f6d18170f
sumafujiko <sumafujiko@gmail.com>
2024-02-19 22:25:41 +0000

    Created an app to whether a number is pos or neg

---
+++ Positivenegative.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class Positivenegative{
+    public static void main(String[] args) {
+        //variables
+        int userNumber;
+        //inputs
+        userNumber = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter a number."));
+        //output
+        if (userNumber>0) {
+            JOptionPane.showMessageDialog(null, "Your number is positive.");
+        } else {
+            JOptionPane.showMessageDialog(null, "Your number is negative.");
+        }
+    }
+}
